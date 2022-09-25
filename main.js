33d59568f65c9f496860f9f61535fd5de481675c
InesRocketDigital <inescastelltort@gmail.com>
2022-09-25 18:58:37 +0200

    ADD Dynamic lines of code

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-// // SLIDER OF THE STEPS OF PLANTING THE AVOCADO //
-
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("slides");
-//   if (n > x.length) {slideIndex = 1} 
-//   if (n < 1) {slideIndex = x.length} ;
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none"; 
-//   }
-//   x[slideIndex-1].style.display = "block"; 
-// }
+function createNumbers() {
+  // Get dynamic number of lines for the coding content
+  var codeContent = document.getElementById('coding-content');
+  // Leave more space for mobile devices
+  let extraHeight = 24;
+  const screenWidth = screen.width;
+  if (screenWidth < 500) {
+    extraHeight = 240; 
+  }
+  var divHeight = codeContent.offsetHeight + extraHeight;
+  var lineHeight = 24;
+  var lines = divHeight / lineHeight;
+  // Add the number of pertinent lines to the numbers div
+  // 1. Get the dic element
+  var codeLines = document.getElementById('coding-lines');
+  // 2. Create the dynamic object
+  let textBlock = document.createElement('p')
+  let textNumbers = ""
+  for (let i = 0; i < lines; i++) {
+    textNumbers += 1 + i + "<br>";
+  }
+  textBlock.innerHTML = textNumbers;
+  // 3. Add dynamic block into website
+  codeLines.appendChild(textBlock);
+}
